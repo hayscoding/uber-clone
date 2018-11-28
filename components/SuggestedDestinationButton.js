321e738c600cf7c6c5fa63cbddd45b7248b1eaea
HaysS <stanfordhays@gmail.com>
2018-11-28 17:01:25 -0600

    refactored styles for SuggestedDest button

--- components/SuggestedDestinationButton.js
+++ components/SuggestedDestinationButton.js
@@ -14,32 +14,13 @@ export const SuggestedDestinationButton = function(props) {
   const cb = props.cb ? props.cb : console.log('Callback function not passed to SuggestedDestinationButton()')
 
   return(
-    <TouchableOpacity 
-      onPress={() => { cb() }}
-      style={{
-        zIndex: 9,
-        position: 'absolute',
-        flexDirection: 'row',
-        width: (WIDTH-40), //40 because of left property multiplied by 2
-        height: 55,
-        top: 170,
-        left: 20,
-        borderBottomLeftRadius: 10,
-        borderBottomRightRadius: 10,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        shadowColor: '#000000',
-        elevation: 3,
-        shadowRadius: 5,
-        shadowOpacity: 1.0,
-      }}
-    >
-      <View style={{flex: 1, alignItems: 'center',}}>
+    <TouchableOpacity style={styles.container} onPress={() => { cb() }}>
+      <View style={styles.locationIcon}>
         <MaterialIcon name="location-on" color="gray" size={15} />
       </View>
       <View style={{flex: 5}}>
-        <Text style={{fontFamily: 'sans-serif', fontSize: 15, color: "#545454"}}>Suggested Address</Text>
-        <Text style={{fontFamily: 'sans-serif', fontSize: 13, color: "#9b9b9b"}}>City, State</Text>
+        <Text style={styles.address}>Suggested Address</Text>
+        <Text style={styles.city}>City, State</Text>
       </View>
     </TouchableOpacity>
   )
@@ -48,7 +29,34 @@ export const SuggestedDestinationButton = function(props) {
 
 const styles = StyleSheet.create({
   container: {
+    zIndex: 9,
+    position: 'absolute',
+    flexDirection: 'row',
+    width: (WIDTH-40), //40 because of left property multiplied by 2 (essentially padding)
+    height: 55,
+    top: 170,
+    left: 20,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    shadowColor: '#000000',
+    elevation: 3,
+    shadowRadius: 5,
+    shadowOpacity: 1.0,
   },
-  menuIcon: {
+  locationIcon: {
+    flex: 1, 
+    alignItems: 'center',
   },
+  address: {
+    fontFamily: 'sans-serif', 
+    fontSize: 15,
+    color: "#545454"
+  },
+  city: {
+    fontFamily: 'sans-serif', 
+    fontSize: 13, 
+    color: "#9b9b9b"
+  }
 });
