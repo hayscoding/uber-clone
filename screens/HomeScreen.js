e6e0edc374e31a5591c9b57e99d6c48fc79d6e91
HaysS <stanfordhays@gmail.com>
2018-11-27 19:52:03 -0600

    added currentLocation button. Does nothing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,12 +12,39 @@ import {
 import { DrawerActions } from 'react-navigation-drawer';
 import { WebBrowser, MapView } from 'expo';
 import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 import { MonoText } from '../components/StyledText';
 import { DestinationButton } from '../components/DestinationButton';
 
 const WIDTH = Dimensions.get('window').width
 const HEIGHT = Dimensions.get('window').height
+
+function CurrentLocationButton() {
+  return(
+    <View style={{zIndex: 9, 
+      position: 'absolute', 
+      width: 45, 
+      height: 45, 
+      backgroundColor: '#fff',
+      top: HEIGHT-140,
+      left: WIDTH-70,
+      borderRadius: 50,
+      shadowColor: '#000000',
+      elevation: 7,
+      shadowRadius: 5,
+      shadowOpacity: 1.0,
+      justifyContent: 'space-around',
+      alignItems: 'center'
+    }}>
+    <MaterialIcon name="my-location" color="#000000" size={25} 
+      style={{
+      }}
+      onPress={() => {}}
+    />
+    </View>
+  )
+}
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -31,6 +58,7 @@ export default class HomeScreen extends React.Component {
           onPress={() => this.props.navigation.dispatch(DrawerActions.openDrawer())}
         />
         <DestinationButton />
+        <CurrentLocationButton />
           <MapView
             initialRegion={{
               latitude: 37.78825,
