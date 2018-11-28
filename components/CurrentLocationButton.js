434466a6f7ec01119c8a1aeeea4eb00178d6cab8
HaysS <stanfordhays@gmail.com>
2018-11-28 16:19:21 -0600

    px from bottom is now passed as prop to CurrentLocationButton. defaults to 60px from bottom

--- components/CurrentLocationButton.js
+++ components/CurrentLocationButton.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   View,
   Dimensions,
+  StyleSheet
 } from 'react-native';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
@@ -10,29 +11,29 @@ const HEIGHT = Dimensions.get('window').height
 
 export const CurrentLocationButton = function(props) {
   const cb = props.cb ? props.cb : console.log('Callback function not passed to CurrentLocationButton()')
-  const rideSectionOpen = props.rideSectionOpen ? props.rideSectionOpen : false;
+  const bottom = props.bottom ? props.bottom : 65;
 
   return(
-    <View style={{zIndex: 9, 
-      position: 'absolute', 
-      width: 45, 
-      height: 45, 
-      backgroundColor: '#fff',
-      top: HEIGHT-140,
-      left: WIDTH-70,
-      borderRadius: 50,
-      shadowColor: '#000000',
-      elevation: 7,
-      shadowRadius: 5,
-      shadowOpacity: 1.0,
-      justifyContent: 'space-around',
-      alignItems: 'center'
-    }}>
-	    <MaterialIcon name="my-location" color="#000000" size={25} 
-	      style={{
-	      }}
-	      onPress={() => { cb() }}
-	    />
+    <View style={[styles.container, {top: HEIGHT-bottom}]}>
+	    <MaterialIcon name="my-location" color="#000000" size={25} onPress={() => { cb() }} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+  	zIndex: 9, 
+    position: 'absolute', 
+    width: 45, 
+    height: 45, 
+    backgroundColor: '#fff',
+    left: WIDTH-70,
+    borderRadius: 50,
+    shadowColor: '#000000',
+    elevation: 7,
+    shadowRadius: 5,
+    shadowOpacity: 1.0,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+});
