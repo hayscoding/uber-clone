7e2036c95f8bd29d4957474d392bafa50466b286
HaysS <stanfordhays@gmail.com>
2018-11-27 19:34:07 -0600

    refactored homescreen styles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,8 +9,8 @@ import {
   View,
   Dimensions,
 } from 'react-native';
-import { WebBrowser, MapView } from 'expo';
 import { DrawerActions } from 'react-navigation-drawer';
+import { WebBrowser, MapView } from 'expo';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { MonoText } from '../components/StyledText';
@@ -27,7 +27,7 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Icon name="md-menu" color="#000000" size={35} style={{zIndex: 9, position: 'absolute', top: 40, left: 20}}
+        <Icon name="md-menu" color="#000000" size={35} style={styles.menuIcon}
           onPress={() => this.props.navigation.dispatch(DrawerActions.openDrawer())}
         />
         <DestinationButton />
@@ -38,7 +38,7 @@ export default class HomeScreen extends React.Component {
               latitudeDelta: 0.0922,
               longitudeDelta: 0.0421,
             }}
-            style={{width: WIDTH, height: HEIGHT, zIndex: 0}}
+            style={styles.map}
           />
       </View>
     );
@@ -50,4 +50,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  menuIcon: {
+    zIndex: 9, 
+    position: 'absolute', 
+    top: 40, 
+    left: 20,
+  },
+  map: {
+    width: WIDTH, 
+    height: HEIGHT, 
+    zIndex: 0,
+  }
 });
