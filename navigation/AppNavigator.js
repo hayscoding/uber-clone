143bb69acec3d61c3295897e964844964c6b1228
HaysS <stanfordhays@gmail.com>
2018-11-29 14:11:37 -0600

    deleted MainTabNavigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
 import MainDrawerNavigator from './MainDrawerNavigator';
 
 export default createSwitchNavigator({
