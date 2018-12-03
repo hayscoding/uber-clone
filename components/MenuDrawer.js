d17c5dab6e54487acd1ea878917170250fc41321
HaysS <stanfordhays@gmail.com>
2018-12-03 15:49:20 -0600

    added links & updates styles in menu drawer

--- components/MenuDrawer.js
+++ components/MenuDrawer.js
@@ -6,6 +6,7 @@ import {
   ScrollView,
   View, 
   Text, 
+  TouchableOpacity,
 } from 'react-native';
 
 const WIDTH = Dimensions.get('window').width
@@ -18,24 +19,64 @@ export default class MenuDrawer extends React.Component {
     return (
       <View style={styles.container}>
         <ScrollView style={{flex: 1,}}>
-          <View style={{flex: 1}}>
+          <View style={{flex: 1, height: 250,}}>
           </View>
-          <View style={{flex: 1, backgroundColor: 'white'}}>
-            <Text
-              onPress={() => navigation.navigate('Home')}
-              style={styles.uglyDrawerItem}>
-              Home
-            </Text>
-            <Text
-              onPress={() => navigation.navigate('Links')}
-              style={styles.uglyDrawerItem}>
-              Links
-            </Text>
-            <Text
-              onPress={() => navigation.navigate('Settings')}
-              style={styles.uglyDrawerItem}>
-              Settings  
-            </Text>
+          <View style={{flex: 1, backgroundColor: 'white', paddingTop: 10}}>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text
+                style={styles.link}>
+                Home
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text
+                onPress={() => navigation.navigate('Links')}
+                style={styles.link}>
+                Your Trips
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text
+                onPress={() => navigation.navigate('Settings')}
+                style={styles.link}>
+                Help  
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text
+                onPress={() => navigation.navigate('Settings')}
+                style={styles.link}>
+                Payment  
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text
+                onPress={() => navigation.navigate('Settings')}
+                style={styles.link}>
+                Ride Pass  
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text
+                onPress={() => navigation.navigate('Settings')}
+                style={styles.link}>
+                Send a Gift  
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text
+                onPress={() => navigation.navigate('Settings')}
+                style={styles.link}>
+                Free Rides  
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text
+                onPress={() => navigation.navigate('Settings')}
+                style={styles.link}>
+                Settings  
+              </Text>
+            </TouchableOpacity>
           </View>
         </ScrollView>
         
@@ -48,17 +89,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    paddingTop: 40,
   },
-  uglyDrawerItem: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#E73536',
-    padding: 15,
+  link: {
+    flex: 1, 
+    fontSize: 20,
+    padding: 6,
+    paddingLeft: 10,
     margin: 5,
-    borderRadius: 2,
-    borderColor: '#E73536',
-    borderWidth: 1,
-    textAlign: 'center'
-  }
+    textAlign: 'left',
+  },
 })
