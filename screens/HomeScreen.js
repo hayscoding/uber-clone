2ec244eb564a73a0d2076390901d49531bd4f3ab
HaysS <stanfordhays@gmail.com>
2018-12-08 22:43:06 -0600

    animateMarkerThruCoords() now loops continuously

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -105,12 +105,16 @@ export default class HomeScreen extends React.Component {
   }
 
   animateMarkerThruCoords(index, coords) {
-    var nextCoords = coords
+    // var nextCoords = coords
     // nextCoords = nextCoords.slice(1, nextCoords.length) //remove first elem
-    console.log('ANIMATE MARKER THRU() COORDS: ', index, coords)
+    // console.log('ANIMATE MARKER THRU() COORDS: ', index, coords, '\nNextCoords: ', coords.slice(1, coords.length)) //remove first elem)
 
+    // if(coords.length != 0)
+    console.log('Coords: ', coords)
     if(coords.length != 0)
-      this.animateMarker(index, coords[1], () => {})
+      this.animateMarker(index, coords[0], () => { this.animateMarkerThruCoords(index, coords.slice(1, coords.length)) })
+    else 
+      this.animateMarkerThruCoords(index, this.state.polylines[index])
   }
 
   animateMarker(index, coord, cb) {
@@ -120,7 +124,7 @@ export default class HomeScreen extends React.Component {
       longitude: coord.longitude
     };
 
-    this.state.markerCoordinates[index].timing(newCoordinate).start(() => {  });
+    this.state.markerCoordinates[index].timing(newCoordinate).start(() => { cb() });
   }
 
   startMarkerAnimation(index, route) {
@@ -141,7 +145,7 @@ export default class HomeScreen extends React.Component {
   startAnimation() {
     if(this.state.route != null) {
       console.log('STARTANIMATION() ROUTE: ', this.state.route.length, '\nFIRST COORD: ', this.state.route[0])
-      this.animateThruCoords(this.state.route)
+      this.animateThruCoords(0, this.state.route)
     }
   }
 
