fe95350818da8c728f60f844b2a4cd5362100fc9
HaysS <stanfordhays@gmail.com>
2018-12-09 19:08:27 -0600

    markerBearings now init in state

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -62,105 +62,11 @@ export default class HomeScreen extends React.Component {
       animating: false,
     };
   }
-  
 
   componentDidMount() {
     DirectionsAPI.getSimulatorPolylines((polylines) => {
-      // console.log('polylines: ', polylines)
-      this.setState({polylines: polylines, markerCoordinates: this.getCoordinateFromPolylines(polylines)})
+      this.setState({polylines: polylines, markerCoordinates: this.getCoordinateFromPolylines(polylines), markerBearings: this.getInitBearings(polylines)})
     })
-  }
-
-  //Iterates through all polylines & returns an array of the 1st coordinate for each polyline
-  getCoordinateFromPolylines(polylines) {
-    var coordinates = []
-
-    polylines.forEach((polyline) => {
-      coordinates.push(new MapView.AnimatedRegion({
-        latitude: polyline[0].latitude,
-        longitude: polyline[0].longitude,
-        latitudeDelta: 0.045,
-        longitudeDelta: 0.045,
-      }))
-    })
-
-    // console.log('setCoords return: ', coordinates)
-    return coordinates
-  }
-
-  animate(coord, cb) {
-    // console.log('ANIMATE() COORDS:\nlat: ', coord.latitude, '\n: ', coord.longitude)
-    // this.state.acc++
-    // console.log('ANIMATE CALLED: ', this.state.acc)
-
-    const newCoordinate = {
-      latitude: coord.latitude,
-      longitude: coord.longitude
-    };
-
-    this.state.coordinate.timing(newCoordinate).start(() => { cb() });
-  }
-
-  animateMarkersThruCoords() {
-    for(var i = 0; i < this.state.polylines.length; i++) {
-      this.animateMarkerThruCoords(i, this.state.polylines[i])
-    }
-  }
-
-  animateMarkerThruCoords(index, coords) {
-    // console.log('INDEX: ', index)
-    // var nextCoords = coords
-    // nextCoords = nextCoords.slice(1, nextCoords.length) //remove first elem
-    // console.log('ANIMATE MARKER THRU() COORDS: ', index, coords, '\nNextCoords: ', coords.slice(1, coords.length)) //remove first elem)
-
-    // if(coords.length != 0)
-    // console.log('Coords: ', coords)
-    if(coords.length != 0){
-      this.getMarkerBearing(coords[0], coords[1])
-
-      this.animateMarker(index, coords[0], () => { this.animateMarkerThruCoords(index, coords.slice(1, coords.length)) })
-    }
-    else 
-      this.animateMarkerThruCoords(index, this.state.polylines[index])
-  }
-
-  updateMarkerBearing(index, bearing) {
-    if(this.state.markerBearing[index] == undefined)
-    this.setState({markerBearings: {index: index, bearing: bearing}})
-  }
-
-  animateMarker(index, coord, cb) {
-    // console.log('animateMarker', coord)
-    const nextCoord = {
-      latitude: coord.latitude,
-      longitude: coord.longitude
-    };
-
-    this.state.markerCoordinates[index].timing(nextCoord).start(() => { cb() });
-  }
-
-  startMarkerAnimation() {
-    // console.log('START MARKER ANIMATIONS(): ', index, route)
-    if(!this.state.animating){
-      this.animateMarkersThruCoords()
-      this.setState({animating: true})
-    }
-  }
-
-  animateThruCoords(coords) {
-    var nextCoords = coords
-    nextCoords = nextCoords.slice(1, nextCoords.length) //remove first elem
-    // console.log('COORDS LENGTH: ', coords.length, 'NEXT COORDS LENGTH: ', nextCoords.length)
-
-    if(coords.length != 0)
-      this.animate(coords[0], () => { this.animateThruCoords(nextCoords) })
-  }
-
-  startAnimation() {
-    if(this.state.route != null) {
-      console.log('STARTANIMATION() ROUTE: ', this.state.route.length, '\nFIRST COORD: ', this.state.route[0])
-      this.animateThruCoords(0, this.state.route)
-    }
   }
 
   componentWillMount() {
@@ -210,6 +116,102 @@ export default class HomeScreen extends React.Component {
     this.setState({destInputOpen: !this.state.destInputOpen})
   }
 
+  getInitBearings(polylines) {
+    const bearings = []
+
+    for(var i = 0; i < polylines.length; i++)
+      bearings.push('90deg')
+
+    return bearings
+  }
+
+  //Iterates through all polylines & returns an array of the 1st coordinate for each polyline
+  getCoordinateFromPolylines(polylines) {
+    var coordinates = []
+
+    polylines.forEach((polyline) => {
+      coordinates.push(new MapView.AnimatedRegion({
+        latitude: polyline[0].latitude,
+        longitude: polyline[0].longitude,
+        latitudeDelta: 0.045,
+        longitudeDelta: 0.045,
+      }))
+    })
+
+    // console.log('setCoords return: ', coordinates)
+    return coordinates
+  }
+
+  animate(coord, cb) {
+    // console.log('ANIMATE() COORDS:\nlat: ', coord.latitude, '\n: ', coord.longitude)
+    // this.state.acc++
+    // console.log('ANIMATE CALLED: ', this.state.acc)
+
+    const newCoordinate = {
+      latitude: coord.latitude,
+      longitude: coord.longitude
+    };
+
+    this.state.coordinate.timing(newCoordinate).start(() => { cb() });
+  }
+
+  animateMarkersThruCoords() {
+    for(var i = 0; i < this.state.polylines.length; i++) {
+      this.animateMarkerThruCoords(i, this.state.polylines[i])
+    }
+  }
+
+  animateMarkerThruCoords(index, coords) {
+    // console.log('INDEX: ', index)
+    // var nextCoords = coords
+    // nextCoords = nextCoords.slice(1, nextCoords.length) //remove first elem
+    // console.log('ANIMATE MARKER THRU() COORDS: ', index, coords, '\nNextCoords: ', coords.slice(1, coords.length)) //remove first elem)
+
+    // if(coords.length != 0)
+    // console.log('Coords: ', coords)
+    if(coords.length != 0){
+      this.updateMarkerBearing(index, this.getBearing(coords[0], coords[1]))
+
+      this.animateMarker(index, coords[0], () => {/* this.animateMarkerThruCoords(index, coords.slice(1, coords.length)) */})
+    }
+    else 
+      this.animateMarkerThruCoords(index, this.state.polylines[index])
+  }
+
+  animateMarker(index, coord, cb) {
+    // console.log('animateMarker', coord)
+    const nextCoord = {
+      latitude: coord.latitude,
+      longitude: coord.longitude
+    };
+
+    this.state.markerCoordinates[index].timing(nextCoord).start(() => { cb() });
+  }
+
+  startMarkerAnimation() {
+    // console.log('START MARKER ANIMATIONS(): ', index, route)
+    if(!this.state.animating){
+      this.animateMarkersThruCoords()
+      this.setState({animating: true})
+    }
+  }
+
+  animateThruCoords(coords) {
+    var nextCoords = coords
+    nextCoords = nextCoords.slice(1, nextCoords.length) //remove first elem
+    // console.log('COORDS LENGTH: ', coords.length, 'NEXT COORDS LENGTH: ', nextCoords.length)
+
+    if(coords.length != 0)
+      this.animate(coords[0], () => { this.animateThruCoords(nextCoords) })
+  }
+
+  startAnimation() {
+    if(this.state.route != null) {
+      console.log('STARTANIMATION() ROUTE: ', this.state.route.length, '\nFIRST COORD: ', this.state.route[0])
+      this.animateThruCoords(0, this.state.route)
+    }
+  }
+
   mainButtons() {
     return(
       <View>
@@ -238,12 +240,18 @@ export default class HomeScreen extends React.Component {
       return this.mainButtons()
   }
 
-  getMarkerBearing(fromCoord, toCoord) {
+  updateMarkerBearing(index, bearing) {
+    console.log('updateMarkerBearing: ', index, this.state.markerBearings[0])
+    // if( == undefined)
+    // this.setState({markerBearings: {index: index, bearing: bearing}})
+  }
+
+  getBearing(fromCoord, toCoord) {
     function toDegrees (radians) {
       return radians * (180 / Math.PI);
     }
 
-    console.log('getMarkerBearing:\nfromCoord: ', fromCoord, '\ntoCoord: ', toCoord)
+    // console.log('getMarkerBearing:\nfromCoord: ', fromCoord, '\ntoCoord: ', toCoord)
     const λ1 = fromCoord.latitude
     const φ1 = fromCoord.longitude
     const λ2 = toCoord.latitude
@@ -256,6 +264,8 @@ export default class HomeScreen extends React.Component {
 
     // console.log('Math.sin:', Math.sin(λ2-λ1))
     // console.log('λ1: ', λ1, 'φ1: ', φ1, '\nλ2: ', λ2, 'φ2: ', φ2, '\nx: ', x, 'y: ', y, '\nBearing: ', bearing)
+    // console.log('Bearing: ', bearing)
+    return bearing
   }
 
   animatedMarker(index) {
@@ -266,7 +276,7 @@ export default class HomeScreen extends React.Component {
           coordinate={this.state.markerCoordinates[index]}
           anchor={{x: 0.35, y: 0.32}} //centers car.png image
           // ref={marker => { this.marker = marker; }}
-          style={{width: 50, height: 50, transform: [{rotate: '90deg'}]}}
+          style={{width: 50, height: 50, transform: [{rotate: '-57deg'}]}}
           //rotation={}
           tracksViewChanges={true}
           //animateMarkerToCoordinate={}
