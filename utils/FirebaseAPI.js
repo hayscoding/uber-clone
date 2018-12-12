993147fb848858353e31d8898ce123e837088677
HaysS <stanfordhays@gmail.com>
2018-12-12 12:30:10 -0600

    added LoginScreen.js

--- utils/FirebaseAPI.js
+++ utils/FirebaseAPI.js
@@ -4,3 +4,16 @@ var env = process.env.NODE_ENV || 'development';
 var config = require('../config')[env];
 
 const apiKey = config.firebase.key
+
+
+// var phoneNumber = getPhoneNumberFromUserInput();
+// var appVerifier = window.recaptchaVerifier;
+// firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+//     .then(function (confirmationResult) {
+//       // SMS sent. Prompt user to type the code from the message, then sign the
+//       // user in with confirmationResult.confirm(code).
+//       window.confirmationResult = confirmationResult;
+//     }).catch(function (error) {
+//       // Error; SMS not sent
+//       // ...
+//     });
