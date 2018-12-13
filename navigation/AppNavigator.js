305590fdbbb1b8bd254f81a9ccea7ac0e837a66c
HaysS <stanfordhays@gmail.com>
2018-12-12 18:19:01 -0600

    loginScreen is now the initial route of app navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,7 +4,13 @@ import { createSwitchNavigator } from 'react-navigation';
 import MainDrawerNavigator from './MainDrawerNavigator';
 import LoginNavigator from './LoginNavigator';
 
+const Config = {
+	initialRouteName: 'Login'
+}
+
 export default createSwitchNavigator({
-  Main: MainDrawerNavigator,
-  Login: LoginNavigator,
-});
+	  Main: MainDrawerNavigator,
+	  Login: LoginNavigator,
+	},
+	Config
+);
