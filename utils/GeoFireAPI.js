e648b78fec04de59d3d76278c36b57d8bc1bb4b6
HaysS <stanfordhays@gmail.com>
2018-12-23 20:30:37 -0600

    added GeoFireAPI

---
+++ utils/GeoFireAPI.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase'
+import geofire from 'geofire'
+
+//Automatically set to 'production' when published through Expo
+var env = process.env.NODE_ENV || 'development';
+// var env = 'production'
+var config = require('../config')[env];
+
+const firebaseRef = firebase.database().ref();
+const geoFire = new GeoFire(firebaseRef);
+
+/*
+#######################################################
+--------------     DATABASE FUNCTIONS    --------------
+#######################################################
+*/
+
+
+
+/*
+#######################################################
+--------------      QUERY FUNCTIONS      --------------
+#######################################################
+*/
+
+export function createQueryAtLocation(lat, lon) {
+
+}
